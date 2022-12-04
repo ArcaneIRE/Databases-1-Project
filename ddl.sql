368073b0e1b64c90135bfbd84397be7818ceb006
Kai Morciniec <kaimorciniec@gmail.com>
2022-12-04 23:20:18 +0000

    Fix small errors

--- ddl.sql
+++ ddl.sql
@@ -23,10 +23,10 @@ CREATE TABLE
 
 CREATE TABLE
     Restaurants (
-        restaurant_id INTEGER PRIMARY KEY,
+        id INTEGER PRIMARY KEY,
         phone_number VARCHAR2(15 CHAR) NOT NULL,
         address_id INTEGER NOT NULL,
-        CONSTRAINT restauraunt_address_fk FOREIGN KEY (address_id) REFERENCES Addresses (id)
+        CONSTRAINT restaurant_address_fk FOREIGN KEY (address_id) REFERENCES Addresses (id)
     );
 
 CREATE TABLE
@@ -40,7 +40,7 @@ CREATE TABLE
 
 CREATE TABLE
     Staff (
-        id INTEGER NOT NULL,
+        id INTEGER PRIMARY KEY,
         person_id INTEGER NOT NULL,
         role VARCHAR2(20 CHAR) NOT NULL CHECK (role IN ('waiter', 'manager')),
         start_date DATE NOT NULL,
@@ -59,23 +59,23 @@ CREATE TABLE
 CREATE TABLE
     BookingReceipts (
         id INTEGER PRIMARY KEY,
-        timestamp TIMESTAMP default CURRENT_TIMESTAMP,
+        timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
         num_people INTEGER NOT NULL,
         booking_method VARCHAR2(20 CHAR) NOT NULL,
         dining_table_id INTEGER NOT NULL,
         booking_customer_id INTEGER NOT NULL,
         waiter_id INTEGER NOT NULL,
         manager_id INTEGER NOT NULL,
-        CONSTRAINT bookingreceipt_waiter_fk FOREIGN KEY (waiter_id) REFERENCES People (id),
-        CONSTRAINT bookingreceipt_manager_fk FOREIGN KEY (waiter_id) REFERENCES People (id),
+        CONSTRAINT bookingreceipt_waiter_fk FOREIGN KEY (waiter_id) REFERENCES Staff (id),
+        CONSTRAINT bookingreceipt_manager_fk FOREIGN KEY (manager_id) REFERENCES Staff (id),
         CONSTRAINT bookingreceipt_diningtable_fk FOREIGN KEY (dining_table_id) DiningTables (id),
         CONSTRAINT bookingreceipt_bookingcustomer_fk FOREIGN KEY (booking_customer_id) Customers (id)
     );
 
 CREATE TABLE
-    BookingCustomers (
-        booking_id INT NOT NULL,
-        customer_id INT NOT NULL,
+    BookingGuests (
+        booking_id INTEGER NOT NULL,
+        customer_id INTEGER NOT NULL,
         CONSTRAINT bc_booking_fk FOREIGN KEY (booking_id) REFERENCES BookingReceipts (id),
         CONSTRAINT bc_customer_fk FOREIGN KEY (customer_id) REFERENCES Customers (id),
         PRIMARY KEY (booking_id, customer_id)
