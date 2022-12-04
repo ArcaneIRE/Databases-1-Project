2de79b3683ab5bce1b661a55bd79c05b08e917f1
Kai Morciniec <kaimorciniec@gmail.com>
2022-12-04 20:19:45 +0000

    Add Person table

--- ddl.sql
+++ ddl.sql
@@ -9,3 +9,14 @@ CREATE TABLE
         postcode VARCHAR2(20 CHAR) NOT NULL,
         country VARCHAR2(20 CHAR) NOT NULL,
     );
+
+CREATE TABLE
+    People (
+        id INTEGER PRIMARY KEY,
+        person_name VARCHAR2(20 CHAR) NOT NULL,
+        dob DATE NOT NULL,
+        email VARCHAR2(255 CHAR) NOT NULL,
+        phone_number VARCHAR2(15 CHAR) NOT NULL,
+        address_id INTEGER NOT NULL,
+        CONSTRAINT fk_address_id FOREIGN KEY (address_id) REFERENCES Addresses (id)
+    );
