213f4417ef2de7ebd9b8947bdcbbf04ead5cb3a0
ArcaneIRE <44246360+ArcaneIRE@users.noreply.github.com>
2022-12-05 20:07:20 +0000

    Add dummy addresses

---
+++ data_insert.sql
@@ -0,0 +1,283 @@
+ -- 20 Total Addresses:
+-- 5 for customers
+-- 10 for staff
+-- 5 for restaurants
+INSERT INTO
+    Addresses
+VALUES
+    (
+        1,
+        '5 Broad Street',
+        'Fairview',
+        'Dublin 3',
+        NULL,
+        'Dublin',
+        'D03 6JZ',
+        'Ireland'
+    );
+
+INSERT INTO
+    Addresses
+VALUES
+    (
+        2,
+        '6 Broad Street',
+        'Fairview',
+        'Dublin 3',
+        NULL,
+        'Dublin',
+        'D03 6LZ',
+        'Ireland'
+    );
+
+INSERT INTO
+    Addresses
+VALUES
+    (
+        3,
+        '7 Broad Street',
+        'Fairview',
+        'Dublin 3',
+        NULL,
+        'Dublin',
+        'D03 6KZ',
+        'Ireland'
+    );
+
+INSERT INTO
+    Addresses
+VALUES
+    (
+        4,
+        '8 Broad Street',
+        'Fairview',
+        'Dublin 3',
+        NULL,
+        'Dublin',
+        'D03 6JU',
+        'Ireland'
+    );
+
+INSERT INTO
+    Addresses
+VALUES
+    (
+        5,
+        '9 Broad Street',
+        'Fairview',
+        'Dublin 3',
+        NULL,
+        'Dublin',
+        'D03 JZ6',
+        'Ireland'
+    );
+
+INSERT INTO
+    Addresses
+VALUES
+    (
+        6,
+        '10 Broad Street',
+        'Fairview',
+        'Dublin 3',
+        NULL,
+        'Dublin',
+        'D03 66Z',
+        'Ireland'
+    );
+
+INSERT INTO
+    Addresses
+VALUES
+    (
+        7,
+        '11 Broad Street',
+        'Fairview',
+        'Dublin 3',
+        NULL,
+        'Dublin',
+        'D03 99Z',
+        'Ireland'
+    );
+
+INSERT INTO
+    Addresses
+VALUES
+    (
+        8,
+        '12 Broad Street',
+        'Fairview',
+        'Dublin 3',
+        NULL,
+        'Dublin',
+        'D03 22Z',
+        'Ireland'
+    );
+
+INSERT INTO
+    Addresses
+VALUES
+    (
+        9,
+        '13 Broad Street',
+        'Fairview',
+        'Dublin 3',
+        NULL,
+        'Dublin',
+        'D03 0AZ',
+        'Ireland'
+    );
+
+INSERT INTO
+    Addresses
+VALUES
+    (
+        10,
+        '14 Broad Street',
+        'Fairview',
+        'Dublin 3',
+        NULL,
+        'Dublin',
+        'D03 R8Z',
+        'Ireland'
+    );
+
+INSERT INTO
+    Addresses
+VALUES
+    (
+        11,
+        '15 Broad Street',
+        'Fairview',
+        'Dublin 3',
+        NULL,
+        'Dublin',
+        'D03 2YZ',
+        'Ireland'
+    );
+
+INSERT INTO
+    Addresses
+VALUES
+    (
+        12,
+        '16 Broad Street',
+        'Fairview',
+        'Dublin 3',
+        NULL,
+        'Dublin',
+        'D03 NJZ',
+        'Ireland'
+    );
+
+INSERT INTO
+    Addresses
+VALUES
+    (
+        13,
+        '17 Broad Street',
+        'Fairview',
+        'Dublin 3',
+        NULL,
+        'Dublin',
+        'D03 6JM',
+        'Ireland'
+    );
+
+INSERT INTO
+    Addresses
+VALUES
+    (
+        14,
+        '18 Broad Street',
+        'Fairview',
+        'Dublin 3',
+        NULL,
+        'Dublin',
+        'D03 6JB',
+        'Ireland'
+    );
+
+INSERT INTO
+    Addresses
+VALUES
+    (
+        15,
+        '19 Broad Street',
+        'Fairview',
+        'Dublin 3',
+        NULL,
+        'Dublin',
+        'D03 6SZ',
+        'Ireland'
+    );
+
+INSERT INTO
+    Addresses
+VALUES
+    (
+        16,
+        '20 Broad Street',
+        'Fairview',
+        'Dublin 3',
+        NULL,
+        'Dublin',
+        'D03 PJZ',
+        'Ireland'
+    );
+
+INSERT INTO
+    Addresses
+VALUES
+    (
+        17,
+        '21 Broad Street',
+        'Fairview',
+        'Dublin 3',
+        NULL,
+        'Dublin',
+        'D03 61O',
+        'Ireland'
+    );
+
+INSERT INTO
+    Addresses
+VALUES
+    (
+        18,
+        '22 Broad Street',
+        'Fairview',
+        'Dublin 3',
+        NULL,
+        'Dublin',
+        'D03 1KO',
+        'Ireland'
+    );
+
+INSERT INTO
+    Addresses
+VALUES
+    (
+        19,
+        '23 Broad Street',
+        'Fairview',
+        'Dublin 3',
+        NULL,
+        'Dublin',
+        'D03 666',
+        'Ireland'
+    );
+
+INSERT INTO
+    Addresses
+VALUES
+    (
+        20,
+        '24 Broad Street',
+        'Fairview',
+        'Dublin 3',
+        NULL,
+        'Dublin',
+        'D03 JJJ',
+        'Ireland'
+    );
