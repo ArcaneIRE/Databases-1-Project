78c7026087fefd9389f917d5c6e1b3467282b19e
ArcaneIRE <44246360+ArcaneIRE@users.noreply.github.com>
2022-12-06 14:52:22 +0000

    Create DML query file

---
+++ dml.sql
@@ -0,0 +1,11 @@
+ --  Include all the following statements, using comments to highlight when one is
+-- being addressed and include an explanation of their implementation.
+--
+--  One UPDATE or one DELETE combined with a subquery
+--  One selection function CASE/DECODE
+--  One INNER JOIN using a GROUP function
+--  One LEFT OUTER JOIN
+--  One RIGHT OUTER JOIN
+--  One UNION
+--  One INTERSECT
+--  One VIEW
