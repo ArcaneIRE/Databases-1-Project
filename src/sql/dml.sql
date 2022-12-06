95ab2a197d6f7a4138ee63037f5f55e1fc9684f1
ArcaneIRE <44246360+ArcaneIRE@users.noreply.github.com>
2022-12-06 20:49:47 +0000

    Add UPDATE statement w/ subquery

--- src/sql/dml.sql
+++ src/sql/dml.sql
@@ -1,7 +1,22 @@
  --  Include all the following statements, using comments to highlight when one is
 -- being addressed and include an explanation of their implementation.
 --
---  One UPDATE or one DELETE combined with a subquery
+-- One UPDATE or one DELETE combined with a subquery
+--
+-- Updates the number of people in a booking by counting the number of people
+-- associated with that booking in another table
+UPDATE
+    BookingReceipts
+SET
+    BookingReceipts.num_people = (
+        SELECT
+            COUNT(customer_id)
+        FROM
+            BookingGuests
+        WHERE
+            BookingReceipts.id = BookingGuests.booking_id
+    );
+
 --  One selection function CASE/DECODE
 --  One INNER JOIN using a GROUP function
 --  One LEFT OUTER JOIN
