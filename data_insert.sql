e38fac00832d6b53ac5127a2b642d30d7cd8b3e3
ArcaneIRE <44246360+ArcaneIRE@users.noreply.github.com>
2022-12-06 01:56:43 +0000

    Add dummy people

--- data_insert.sql
+++ data_insert.sql
@@ -461,3 +461,288 @@ VALUES
         'D03 JJJ',
         'Ireland'
     );
+
+INSERT INTO
+    People (
+        id,
+        person_name,
+        dob,
+        email,
+        phone_number,
+        address_id
+    )
+VALUES
+    (
+        1,
+        'John Doe',
+        TO_DATE('2000-01-01', 'yyyy-mm-dd'),
+        'johndoe@example.com',
+        '5555551212',
+        1
+    );
+
+INSERT INTO
+    People (
+        id,
+        person_name,
+        dob,
+        email,
+        phone_number,
+        address_id
+    )
+VALUES
+    (
+        2,
+        'Jane Doe',
+        TO_DATE('2000-02-02', 'yyyy-mm-dd'),
+        'janedoe@example.com',
+        '5555551313',
+        2
+    );
+
+INSERT INTO
+    People (
+        id,
+        person_name,
+        dob,
+        email,
+        phone_number,
+        address_id
+    )
+VALUES
+    (
+        3,
+        'James Smith',
+        TO_DATE('2000-03-03', 'yyyy-mm-dd'),
+        'jamessmith@example.com',
+        '5555551414',
+        3
+    );
+
+INSERT INTO
+    People (
+        id,
+        person_name,
+        dob,
+        email,
+        phone_number,
+        address_id
+    )
+VALUES
+    (
+        4,
+        'Emily Johnson',
+        TO_DATE('2000-04-04', 'yyyy-mm-dd'),
+        'emilyjohnson@example.com',
+        '5555551515',
+        4
+    );
+
+INSERT INTO
+    People (
+        id,
+        person_name,
+        dob,
+        email,
+        phone_number,
+        address_id
+    )
+VALUES
+    (
+        5,
+        'Samantha Williams',
+        TO_DATE('2000-05-05', 'yyyy-mm-dd'),
+        'samanthawilliams@example.com',
+        '5555551616',
+        5
+    );
+
+INSERT INTO
+    People (
+        id,
+        person_name,
+        dob,
+        email,
+        phone_number,
+        address_id
+    )
+VALUES
+    (
+        6,
+        'Michael Brown',
+        TO_DATE('2000-06-06', 'yyyy-mm-dd'),
+        'michaelbrown@example.com',
+        '5555551717',
+        6
+    );
+
+INSERT INTO
+    People (
+        id,
+        person_name,
+        dob,
+        email,
+        phone_number,
+        address_id
+    )
+VALUES
+    (
+        7,
+        'Sara Davis',
+        TO_DATE('2000-07-07', 'yyyy-mm-dd'),
+        'saradavis@example.com',
+        '5555551818',
+        7
+    );
+
+INSERT INTO
+    People (
+        id,
+        person_name,
+        dob,
+        email,
+        phone_number,
+        address_id
+    )
+VALUES
+    (
+        8,
+        'William Thompson',
+        TO_DATE('2000-08-08', 'yyyy-mm-dd'),
+        'williamthompson@example.com',
+        '5555551919',
+        8
+    );
+
+INSERT INTO
+    People (
+        id,
+        person_name,
+        dob,
+        email,
+        phone_number,
+        address_id
+    )
+VALUES
+    (
+        9,
+        'Ashley Taylor',
+        TO_DATE('2000-03-03', 'yyyy-mm-dd'),
+        'ashleytaylor@example.com',
+        '5555552020',
+        9
+    );
+
+INSERT INTO
+    People (
+        id,
+        person_name,
+        dob,
+        email,
+        phone_number,
+        address_id
+    )
+VALUES
+    (
+        10,
+        'Richard Anderson',
+        TO_DATE('2000-10-10', 'yyyy-mm-dd'),
+        'richardanderson@example.com',
+        '5555552121',
+        10
+    );
+
+INSERT INTO
+    People (
+        id,
+        person_name,
+        dob,
+        email,
+        phone_number,
+        address_id
+    )
+VALUES
+    (
+        11,
+        'Emily Davis',
+        TO_DATE('2000-11-11', 'yyyy-mm-dd'),
+        'emilydavis@example.com',
+        '5555552222',
+        11
+    );
+
+INSERT INTO
+    People (
+        id,
+        person_name,
+        dob,
+        email,
+        phone_number,
+        address_id
+    )
+VALUES
+    (
+        12,
+        'Daniel Johnson',
+        TO_DATE('2000-12-12', 'yyyy-mm-dd'),
+        'danieljohnson@example.com',
+        '5555552323',
+        12
+    );
+
+INSERT INTO
+    People (
+        id,
+        person_name,
+        dob,
+        email,
+        phone_number,
+        address_id
+    )
+VALUES
+    (
+        13,
+        'Emma Williams',
+        TO_DATE('2000-01-01', 'yyyy-mm-dd'),
+        'emmawilliams@example.com',
+        '5555552424',
+        13
+    );
+
+INSERT INTO
+    People (
+        id,
+        person_name,
+        dob,
+        email,
+        phone_number,
+        address_id
+    )
+VALUES
+    (
+        14,
+        'Michael Thompson',
+        TO_DATE('2000-02-02', 'yyyy-mm-dd'),
+        'michaelthompson@example.com',
+        '5555552525',
+        14
+    );
+
+INSERT INTO
+    People (
+        id,
+        person_name,
+        dob,
+        email,
+        phone_number,
+        address_id
+    )
+VALUES
+    (
+        15,
+        'Sara Taylor',
+        TO_DATE('2000-03-03', 'yyyy-mm-dd'),
+        'sarataylor@example.com',
+        '5555552626',
+        15
+    );
